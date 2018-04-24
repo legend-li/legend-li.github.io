8d3bbc030808e2a921848922e9446159d6a8dd6a
lizeng <lizeng@guanghe.tv.com>
2018-04-24 19:17:56 +0800

    Site updated: 2018-04-24 19:17:56

--- js/posts.js
+++ js/posts.js
@@ -1,1 +1,1 @@
-[{"title":"HelloWorld","date":"2018.04.24","uri":"2018/04/24/HelloWorld/","excerpt":""},{"title":"Hello World","date":"2018.04.24","uri":"2018/04/24/hello-world/","excerpt":""}]
+[{"title":"HelloWorld","date":"2018.04.24","uri":"2018/04/24/HelloWorld/","excerpt":""}]
